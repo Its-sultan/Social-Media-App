f7e77cf14a94fcafe8f196ec6e4c96513921c718
Its-sultan <thabitsultan2002@gmail.com>
2025-06-03 23:51:24 +0300

    Added the loading state to the Login  button and a spinner

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import { useState } from 'react';
-import { FaEnvelope, FaEye, FaEyeSlash, FaLock, FaUser,  } from 'react-icons/fa';
+import { FaEnvelope, FaEye, FaEyeSlash, FaLock, FaRegUserCircle, FaUser,  } from 'react-icons/fa';
+import { ImSpinner, ImSpinner3 } from 'react-icons/im';
 import axios from 'axios';
 import { userActions } from '../stores/user-slice';
 import { useDispatch } from 'react-redux';
@@ -11,6 +12,7 @@ import { toast } from 'react-toastify';
 const Login = () => {
   const [userData, setUserData] = useState({ email: "", password: ""})
   const [error, setError] = useState("")
+  const [loading, setLoading] = useState(false)
   const [showPassword, setShowPassword] = useState(false)
   const navigate =useNavigate()
   const dispatch = useDispatch()
@@ -23,32 +25,54 @@ const Login = () => {
     }))
   }
   // function to login user 
-  const loginUser = async (e) => {
-    e.preventDefault(); 
+  // const loginUser = async (e) => {
+  //   e.preventDefault(); 
 
-    try {
-      await axios.post(`${import.meta.env.VITE_API_URL}/users/login`, userData);
-       if(response.status === "200"){
-        dispatch(userActions.changeCurrentUser(response?.data))
-        localStorage.setItem("currentUser", JSON.stringify(response?.data));
-        toast.success("Login successful!");
-        navigate("/");
-       }
+  //   try {
+  //     await axios.post(`${import.meta.env.VITE_API_URL}/users/login`, userData);
+  //      if(response.status === "200"){
+  //       dispatch(userActions.changeCurrentUser(response?.data))
+  //       localStorage.setItem("currentUser", JSON.stringify(response?.data));
+  //       toast.success("Login successful!");
+  //       navigate("/");
+  //      }
 
-    } catch (error) {
-      toast.error(error.response?.data?.message || "Something went wrong");
-    }
-  }
+  //   } catch (error) {
+  //     toast.error(error.response?.data?.message || "Something went wrong");
+  //   }
+  // }
 
 
   // console.log(userData)
+
+
+   const loginUser = async (e) => {
+    e.preventDefault()
+    setLoading(true)
+
+    try {
+      const response = await axios.post(`${import.meta.env.VITE_API_URL}/users/login`, userData)
+      if (response.status === 200) {
+        dispatch(userActions.changeCurrentUser(response.data))
+        localStorage.setItem("currentUser", JSON.stringify(response.data))
+        toast.success("Login successful!")
+        navigate("/")
+      }
+    } catch (error) {
+      toast.error(error.response?.data?.message || "Something went wrong")
+    } finally {
+      setLoading(false)
+    }
+  }
 
   return (
     <section className=" flex justify-center items-center h-screen bg-gradient-to-r from-gray-100 to-gray-200">
        <div className="register_container bg-white p-8 rounded-lg shadow-lg w-full max-w-md">
     
 
-          <h2 className='text-3xl text-teal-600 font-bold text-center mb-6'>Sign In</h2>
+          <h2 className='text-3xl text-teal-500 font-bold text-center mb-6'>
+            <FaRegUserCircle className="inline-block mr-2 animate-pulse text-4xl text-teal-500 transition-transform duration-300 hover:rotate-12" />
+            Sign In</h2>
 
            {error && <p className="text-red-500 text-sm mb-4 text-center">{error}</p>}
            <form action="" className='space-y-5' onSubmit={loginUser}>
@@ -56,21 +80,35 @@ const Login = () => {
 
               {/* email */}
               <div className="relative">
-                <FaEnvelope className="absolute right-3 top-3 text-gray-400" />
+                <FaEnvelope className="absolute right-3 top-3 text-gray-400 hover:text-teal-500 transition-all duration-300 transform hover:scale-110 hover:rotate-6" />
              <input type="email" name="email" placeholder="Enter Your Email"  id="" onChange=
              {changeInputHandler} className='w-full pr-10 px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-teal-500 '  />
                </div>
 
 
              <div className="password_controller relative">
-              <FaLock className="absolute right-3 top-3 text-gray-400" />
+              <FaLock className="absolute right-3 top-3 text-gray-400 hover:text-teal-500 transition-all duration-300 transform hover:scale-110 hover:rotate-6" />
               <input type={showPassword ? "text" : "password"} name="password" placeholder='Enter Your Password ' id="" 
               onChange={changeInputHandler} className='w-full pr-10 px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-teal-500 ' />
               {/* <span onClick={() => setShowPassword(!showPassword)}  className="absolute right-3 top-2.5 text-gray-500">{showPassword ? <FaEyeSlash /> : <FaEye />}</span> */}
              </div>
 
 
-             <button type="submit" className='btn w-full bg-teal-600 hover:bg-teal-700 text-white py-2 rounded-md transition duration-300'>Login</button>
+             <button type="submit" className=' w-full flex items-center justify-center gap-2 bg-teal-600 hover:bg-teal-700 text-white py-2 rounded-md transition duration-300'
+              disabled={loading}> 
+              
+              {loading ? (
+                <>
+              <ImSpinner3 className="animate-spin text-center " />
+              Signing in...
+              </>
+            ) : (
+              <>
+              Login
+              </>
+            )}
+            
+            </button>
 
              <p className="text-sm mt-4 text-center text-gray-600">Don't have an account? <Link to="/register" className='text-teal-600 hover:underline '>Sign up</Link> </p>
             
